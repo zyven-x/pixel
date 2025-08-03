2643a4a6b71a85e05c0f2be2f0df6451415e6cc4
ARYAN <aryankr9083@gmail.com>
2025-08-04 02:50:44 +0530

    unwanted tag removed - no fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode, useState, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
-import { SessionContextProvider, createBrowserSupabaseClient } from "@supabase/auth-helpers-react";
+import { SessionContextProvider} from "@supabase/auth-helpers-react";
 import { supabase } from "./supabaseClient";
 
 import App from "./App.jsx";
@@ -13,25 +13,6 @@ import ResetPassword from "./pages/ResetPassword.jsx";
 
 import "./index.css";
 
-useEffect(() => {
-  const recoverSession = async () => {
-    const { data } = await supabase.auth.getSession();
-    if (!data.session) {
-      const { data: recoveredSession } = await supabase.auth.recoverSession();
-      if (recoveredSession?.session) {
-        supabase.auth.setSession(recoveredSession.session);
-      }
-    }
-  };
-  recoverSession();
-}, []);
-
-const [supabaseClient] = useState(() =>
-  createBrowserSupabaseClient({
-    persistSession: true,
-    autoRefreshToken: true,
-  })
-);
 const Router = import.meta.env.VITE_IS_EXTENSION === "true" ? HashRouter : BrowserRouter;
 createRoot(document.getElementById("root")).render(
   <StrictMode>
