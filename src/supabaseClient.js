b6ac02e3c837d116eb55f032b698505530ad5495
ARYAN <aryankr9083@gmail.com>
2025-08-04 02:03:28 +0530

    Extension sync fix 1.3

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://bftyqxmxngfawxkfpavx.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmdHlxeG14bmdmYXd4a2ZwYXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2Njg3NzksImV4cCI6MjA2ODI0NDc3OX0.FjFjYBkcveSQDZvLD8Qm5m8Azi6vhqmnaIjEyYWndPQ'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,         
+    autoRefreshToken: true, 
+    detectSessionInUrl: true,
+  }
+});
